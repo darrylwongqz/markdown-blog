a21055278b90f193127610e5331e2ef95c8b9203
darrylwongalta <84469361+darrylwongqz@users.noreply.github.com>
2023-01-16 23:54:08 +0800

    About page

---
+++ pages/about.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+type Props = {};
+
+function About({}: Props) {
+  return (
+    <Layout title="About Devspace">
+      <h1 className="pb-5 text-5xl font-bold border-b-4">About</h1>
+
+      <div className="px-10 py-6 mt-6 bg-white rounded-lg shadow-md">
+        <h3 className="mb-5 text-2xl">DevSpace Blog</h3>
+      </div>
+
+      <p className="mb-3">This is a blog built with Next.js and Markdown</p>
+
+      <p>
+        <span className="font-bold">Version 1.0.0</span>
+      </p>
+    </Layout>
+  );
+}
+
+export default About;
